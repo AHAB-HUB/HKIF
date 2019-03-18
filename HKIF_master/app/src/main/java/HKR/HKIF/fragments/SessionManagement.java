57c0aede37561191b2fff283ac1341c11a350b43
Stephan99 <nnstephan9@yahoo.com>
2019-03-18 20:16:50 +0100

    Changed the entire home fragment and added session management fragment
    You guys have to just add listener on one of the item to swish between users as it was before. As it is right now, only Football has a listener and open Team leader's menu on which i'm working.

---
+++ HKIF_master/app/src/main/java/HKR/HKIF/fragments/SessionManagement.java
@@ -0,0 +1,80 @@
+package HKR.HKIF.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.ramotion.foldingcell.FoldingCell;
+
+import java.util.ArrayList;
+
+import androidx.fragment.app.Fragment;
+
+public class SessionManagement extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+
+        return inflater.inflate(R.layout.session_management, container, false);
+    }
+
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+
+        super.onActivityCreated(savedInstanceState);
+
+
+        // get our list view
+        ListView theListView = getActivity().findViewById(R.id.lvSM);
+
+
+        //TODO get the list info from database and fill the list below
+        // prepare elements to display
+        final ArrayList<Item> items = Item.getTestingList();
+
+
+        //TODO add a loop to create as much buttons we need that implement the same method/function
+        // add custom btn handler to first list item
+        items.get(0).setRequestBtnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity().getApplicationContext(), "CUSTOM HANDLER FOR FIRST BUTTON", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        // create custom adapter that holds elements and their state (we need hold a id's of unfolded elements for reusable elements)
+        final FoldingCellListAdapter adapter = new FoldingCellListAdapter(getActivity(), items);
+
+
+        // add default btn handler for each request btn on each item if custom handler not found
+        adapter.setDefaultRequestBtnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity().getApplicationContext(), "DEFAULT HANDLER FOR ALL BUTTONS", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        // set elements to adapter
+        theListView.setAdapter(adapter);
+
+        // set on click event listener to list view
+        theListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
+                // toggle clicked cell state
+                ((FoldingCell) view).toggle(false);
+                // register in adapter that state for selected cell is toggled
+                adapter.registerToggle(pos);
+            }
+        });
+    }
+
+}
