a754ce163cf3274064474d69dff106d5f12c9f67
Nawar <nawar.aghi0002@stud.hkr.se>
2019-05-08 19:17:05 +0200

    SportList, PersonList and their classes

--- adminHkif/src/main/java/hkr/DatabaseConnector.java
+++ adminHkif/src/main/java/hkr/DatabaseConnector.java
@@ -2,36 +2,35 @@ package hkr;
 
 
 import java.lang.reflect.InvocationTargetException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class DatabaseConnector {
 
+    private Connection connection;
+    private Statement statement;
 
     public DatabaseConnector(){
-        Connection conn = null;
-        Properties properties = new Properties(); //I use Properties to make things easer.
-        properties.setProperty("user", "root");
-        properties.setProperty("password", "YourPassword");
-        properties.setProperty("useSSL", "false"); //Set useSSL to false to solve the problem.
+        dbConnection();
+    }
 
+    private void dbConnection(){
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver").getConstructor().newInstance();
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306", properties);
-
-            // Do something with the Connection
-
-        } catch (SQLException ex) {
-            // handle any errors
-            System.out.println("SQLException: " + ex.getMessage());
-            System.out.println("SQLState: " + ex.getSQLState());
-            System.out.println("VendorError: " + ex.getErrorCode());
-        }catch (ClassNotFoundException | IllegalAccessException | InstantiationException | NoSuchMethodException | InvocationTargetException e) {
-            e.printStackTrace();
+            String url = "jdbc:mysql://localhost:3306/admin_hkif?user=root&password=root";
+            connection = DriverManager.getConnection(url);
+            statement = connection.createStatement();
+            System.out.println("Connected");
+        }catch (SQLException ex){
+            System.out.println("Error in connection");
         }
 
     }
 
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public Statement getStatement() {
+        return statement;
+    }
 }
