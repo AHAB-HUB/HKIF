df7888405cc29fc641a5819702f357902bda3b01
Stephan99 <nnstephan9@yahoo.com>
2019-05-22 16:27:53 +0200

    deleting admin

--- HKIF_master/app/src/main/java/HKR/HKIF/fragments/SignInFragment.java
+++ HKIF_master/app/src/main/java/HKR/HKIF/fragments/SignInFragment.java
@@ -70,7 +70,7 @@ public class SignInFragment extends Fragment implements View.OnClickListener {
         String passwordLog = password.getText().toString().trim();
 
         if (emailLog.isEmpty()) {
-            email.setError("Email must be enter");
+            email.setError("Email must be entered");
             email.requestFocus();
             return;
         }
@@ -122,10 +122,12 @@ public class SignInFragment extends Fragment implements View.OnClickListener {
                                     navigationView1.getMenu().clear();
                                     navigationView1.inflateMenu(R.menu.drawer_navigation_team_leader);
                                     break;
+
                                 case "ADMIN":
                                     navigationView1.getMenu().clear();
-                                    navigationView1.inflateMenu(R.menu.drawer_navigation_admin);
+                                    navigationView1.inflateMenu(R.menu.drawer_navigation_member);
                                     break;
+//
                             }
                         }
 
